9b6ba6a14e01f47a8d04e2760468c4dcba831a35
Crow <devcrowmaster@gmail.com>
2020-03-30 12:58:44 -0500

    Creando componente Form y se crea un custom hook.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+import image from './cryptomonedas.png';
+import Form from './components/Form';
+
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue',cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 3.5rem;
+  margin-top: 5rem;
+  margin-bottom: 3.5rem;
+
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <div>
+        <Image
+          src={image}
+          alt="image crypto"
+        />
+      </div>
+
+      <div>
+        <Heading>Cotiza Criptomonedas al instante</Heading>
+
+
+        <Form/>
+
+      </div>
+      
+    </Container>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Button = styled.input`
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 20px;
+  padding: 10px;
+  background-color: #66a2fe;
+  border: none;
+  width: 100%;
+  border-radius: 10px;
+  color: #fff;
+  transition: background-color .3s ease;
+
+  &:hover{
+    background-color: #326ac0;
+    cursor: pointer;
+  }
+`;
+
+
+const Form = () => {
+  return ( 
+    <form>
+
+      <Button
+        type="submit"
+        value="Calcular"
+      />
+
+    </form>
+  );
+}
+ 
+export default Form;
