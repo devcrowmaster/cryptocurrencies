9c9fcc5b5081a34bcb1242065c3be9fbe413f333
Crow <devcrowmaster@gmail.com>
2020-05-19 16:33:42 -0500

    Updates

---
+++ precache-manifest.241141812353a3b1d7965327655be5ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebef3cb1da843367d949f4f961e62e4",
+    "url": "/cryptocurrencies/index.html"
+  },
+  {
+    "revision": "d8c94b90dd77e6e4b463",
+    "url": "/cryptocurrencies/static/css/main.08b449d4.chunk.css"
+  },
+  {
+    "revision": "fa1e528a5970c8229733",
+    "url": "/cryptocurrencies/static/js/2.f5df8738.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cryptocurrencies/static/js/2.f5df8738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c94b90dd77e6e4b463",
+    "url": "/cryptocurrencies/static/js/main.2b32d227.chunk.js"
+  },
+  {
+    "revision": "428e63197579d68fa486",
+    "url": "/cryptocurrencies/static/js/runtime-main.9a8166b4.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/cryptocurrencies/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
